9ca7d8a76efeef4446e695f00241bd234279d2b8
safinghoghabori <safinghoghabori65@gmail.com>
2024-09-02 09:44:19 +0530

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RestaurantReservationSystemMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
